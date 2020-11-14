3bb08928a812105d69d7d0440eb766b8372463f3
Dimas Lesmana <dimas.lesmana@student.umn.ac.id>
2020-11-14 19:08:04 +0700

    updated program.cs

--- GTA Online block port/Program.cs
+++ GTA Online block port/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NetFwTypeLib;
 
 
 namespace GTA_Online_block_port
@@ -12,16 +7,30 @@ namespace GTA_Online_block_port
     {
         static void Main(string[] args)
         {
-            bool showMenu = true;
-            Console.Title = "GTAO block port";
-            while (showMenu)
+            Console.Title = Firewall.ruleName;
+            while (true)
             {
                 Console.Clear();
                 GC.Collect();
                 GC.WaitForPendingFinalizers();
                 GC.Collect();
-                showMenu = Firewall.Menu();
+                Menu();
             }
+        }
+
+        private static void Menu()
+        {
+            if (!Firewall.isExists())
+                return;
+
+            string status = Firewall.isEnabled() ? "Enabled" : "Disabled";
+
+            Console.WriteLine($"Status       : {status}");
+            Console.WriteLine($"Blocked port : {Firewall.port} (Outbound Rules)");
+            Console.WriteLine($"Protocol     : UDP ({Firewall.protocol})\n");
+            Console.Write("Press any key to toggle . . . ");
+            Console.ReadKey();
+            Firewall.Toggle();
         }
     }
 }
